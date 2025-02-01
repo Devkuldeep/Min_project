1d3d0850f9de69c8af397e461ee6b8c130ea747e
Devkuldeep <kuldeeppatel.dev@gmail.com>
2025-02-01 20:23:34 +0530

    dfds

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,13 @@ function cleanSite() {
 
 // **Task: Copy files from Project 1**
 function copyProject1() {
-  return gulp.src('project1/**/*')
+  return gulp.src('01_gsap_left_text_right_image_container/**/*')
              .pipe(gulp.dest('_site/project1'));
 }
 
 // **Task: Copy files from Project 2**
 function copyProject2() {
-  return gulp.src('project2/**/*')
+  return gulp.src('min_project_1/**/*')
              .pipe(gulp.dest('_site/project2'));
 }
 
